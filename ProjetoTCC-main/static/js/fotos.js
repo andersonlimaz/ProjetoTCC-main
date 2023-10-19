01587b6e815d0f80633ed3d1a386ad1a670093e5
andersonlimaz <andersonlimaz@outlook.com>
2023-10-19 10:33:17 -0300

    commit parte das imagens funcionando

---
+++ ProjetoTCC-main/static/js/fotos.js
@@ -0,0 +1,47 @@
+// function displayImage() {
+//     var selectedValue = document.getElementById("partnerSelect").value;
+//     var image1 = document.getElementById("image1");
+//     var image2 = document.getElementById("image2");
+//     var image3 = document.getElementById("image3");
+
+//     // Esconda todas as imagens
+//     image1.style.display = "none";
+//     image2.style.display = "none";
+//     image3.style.display = "none";
+
+//     // Exiba a imagem com base na opção selecionada
+//     if (selectedValue === "Empresa 1") {
+//         image1.style.display = "block";
+//         image1.src = "{% static 'img/img1.jpg' %}";
+//     } else if (selectedValue === "Empresa 2") {
+//         image2.style.display = "block";
+//         image2.src = "{% static 'img/img2.jpg' %}";
+//     } else if (selectedValue === "Empresa 3") {
+//         image3.style.display = "block";
+//         image3.src = "{% static 'img/img3.jpg' %}";
+//     }
+// }
+
+function displayImage(){
+    var selectedValue = document.getElementById("partnerSelect").value;
+    var image1 = document.getElementById("image1");
+    var image2 = document.getElementById("image2");
+    var image3 = document.getElementById("image3");
+    
+    // Esconda todas as imagens
+    image1.style.display = "none";
+    image2.style.display = "none";
+    image3.style.display = "none";
+
+    // Exiba a imagem com base na opção selecionada
+    if (selectedValue === "Empresa 1") {
+        image1.style.display= "block";
+        image1.src = img1;
+    } else if (selectedValue === "Empresa 2") {
+        image2.style.display= "block"
+        image2.src = img2;
+    } else if (selectedValue === "Empresa 3") {
+        image3.style.display= "block"
+        image3.src = img3;
+    } 
+}
